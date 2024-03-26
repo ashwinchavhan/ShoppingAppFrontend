53b702471527798c3d66e6e77cf830dcb123a1eb
ASHWIN  CHAVHAN <113572808+ashwinchavhan@users.noreply.github.com>
2024-03-26 17:32:20 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,3 @@
-
 import { FaTrashAlt } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-hot-toast';
@@ -13,18 +12,16 @@ const CartItem = ({ item }) => {
   };
 
   return (
-    <div className="flex items-center justify-between p-4 border-b border-gray-300">
-      <div className="flex items-center space-x-4 w-100px">
+    <div className="flex flex-col md:flex-row items-start md:items-center justify-between p-4 border-b border-gray-300">
+      <div className="flex items-start md:items-center space-x-4">
         <img className="h-40 w-40 object-cover" src={item.image} alt={item.title} />
         <div>
           <h1 className="text-lg font-semibold">{item.title}</h1>
-          
-          <p className="text-gray-700 w-40">{item.description}</p>
-          <br/>
+          <p className="text-gray-700 md:w-40">{item.description}</p>
         </div>
       </div>
-      <div className="flex items-center space-x-2">
-        <p>${item.price}</p>
+      <div className="flex items-center mt-4 md:mt-0 md:ml-4 space-x-2">
+        <p className="md:ml-4">${item.price}</p>
         <div onClick={removeFromCart} className="cursor-pointer">
           <FaTrashAlt className="text-red-500" />
         </div>
@@ -34,4 +31,12 @@ const CartItem = ({ item }) => {
 };
 
 export default CartItem;
+
+
+
+
+
+
+
+
 
